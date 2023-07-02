88c044c8bcb70efec176718650575c2a141798b8
SPeak <speakshen@163.com>
2023-07-02 13:11:18 +0800

    pass-by-functor-template: add useage2

--- cpp/pass-by-functor-template.cpp
+++ cpp/pass-by-functor-template.cpp
@@ -17,6 +17,7 @@ struct Functor2 {
 template<typename T, typename... Args>
 struct Functor3 {
     auto /*need cpp14*/ operator()(T t, Args&&... args) /* c11 -> returnType*/ {
+        // auto s = (args + ...); // c++17
         auto s = t + sum(std::forward<Args>(args)...);
         std::cout << "functor3 template: " << s << std::endl;
         return s;
@@ -35,15 +36,42 @@ private:
 };
 
 template <template <typename...> class FT, typename... Args>
-void test(Args&&... args) {
+void test1(Args&&... args) {
     // do something...
     FT<Args...>()(std::forward<Args>(args)...);
     // do something...
 }
 
+template <template <typename> class FT, typename T1, typename T2>
+void test2(T1 t1,  T2 t2) {
+
+    std::cout << std::endl;
+
+    FT<T1>()(t1);
+    FT<T2>()(t2);
+
+    std::cout << std::endl;
+
+    FT<double> printDouble;
+    FT<char>   printChar;
+    FT<short>  printShort;
+
+    printDouble(t1); printDouble(t2);
+    std::cout << std::endl;
+    printChar(t1); printChar(t2);
+    std::cout << std::endl;
+    printShort(t1); printShort(t2);
+}
+
+
 int main() {
-    test<Functor1>(11);
-    test<Functor2>(10, 11);
-    test<Functor3>(2, 1, 4, 2.3, -1, -0.1, '0');
+// useage1
+    test1<Functor1>(11);
+    test1<Functor2>(10, 11);
+    test1<Functor3>(2, 1, 4, 2.3, -1, -0.1, '0');
+
+// useage2:
+    test2<Functor1>(48.1, '0');
+
     return 0;
 }

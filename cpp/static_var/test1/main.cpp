aa10ec2f40d4ceb08c514c80dd2c4fdc9bbcf0b7
SPeak <speakshen@163.com>
2024-05-15 12:10:32 +0800

    cpp: update static var

--- cpp/static_var/test1/main.cpp
+++ cpp/static_var/test1/main.cpp
@@ -5,8 +5,31 @@ int count() {
     return ++cnt;
 }
 
+struct A {
+    A(const char *str) {
+        printf("A: %s\n", str);
+    }
+};
+
+A & create_a1() {
+    static A a1("a1");
+    return a1;
+}
+
+A & create_a2() {
+    static A a2("a2");
+    return a2;
+}
+
+void init_flow_control() {
+    create_a2();
+    create_a1();
+}
+
 int main() {
+
     printf("count: %d\n", count());
     printf("count: %d\n", count());
+
     return 0;
 }

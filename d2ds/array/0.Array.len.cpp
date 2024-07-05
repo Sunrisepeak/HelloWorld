ee3b401eecccb5ffb31ffaf88ae9b30049bea7f7
SPeak <speakshen@163.com>
2024-07-06 07:00:35 +0800

    d2ds: arr-len

--- d2ds/array/0.Array.len.cpp
+++ d2ds/array/0.Array.len.cpp
@@ -1,31 +1,28 @@
 #include <iostream>
 
-/*
-    g++ d2ds/array/0.Array.len.cpp && ./a.out
-*/
-
 template <typename T>
 void process(T arr) {
-    printf("process: len %ld, arr[0] = %d\n", sizeof(arr), arr[0]);
+    printf("process: len %ld, arr[0] = %d \n", sizeof(arr), arr[0]);
 }
 
 template <typename T, unsigned int N>
 struct Array {
-
     T & operator[](int index) {
-        return data[index];
+        return arr[index];
     }
 
-    T data[N];
+    T arr[N];
 };
 
 int main() {
     int arr[3];
-    Array<int, 3> myArr;
-    arr[0] = myArr[0] = 2233;
-    printf("len %ld - arr[0] = %d\n", sizeof(arr), arr[0]);
+    Array <int, 3> myArr;
+
+    arr[0] = myArr[0] = 3;
+
+    printf("len %ld , arr[0] = %d \n", sizeof(arr), arr[0]);
     process(arr);
-    printf("len %ld - arr[0] = %d\n", sizeof(myArr), myArr[0]);
+    printf("len %ld , arr[0] = %d \n", sizeof(myArr), myArr[0]);
     process(myArr);
     return 0;
 }
